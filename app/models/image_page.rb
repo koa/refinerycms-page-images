7f4050b1558c0a9aae66afb4ce914210da0e5a93
Andreas König <andreas.koenig@berg-turbenthal.ch>
2011-05-23 15:14:35 +0200

    support for mass-assignment-protection

--- app/models/image_page.rb
+++ app/models/image_page.rb
@@ -2,7 +2,9 @@ class ImagePage < ActiveRecord::Base
   
   belongs_to :image
   belongs_to :page
-  
+
+  attr_accessible :image_id, :position
+
   translates :caption if self.respond_to?(:translates)
 
 end
